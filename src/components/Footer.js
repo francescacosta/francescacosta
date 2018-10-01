0a71edbdec2258718dad60b35a8d4ca6f815d965
Francesca <francesca@thrivewb.com.au>
2018-10-01 13:01:15 +1000

    update "footer.js"

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,9 +3,9 @@ import React from 'react'
 import './Footer.css'
 
 export default ({ globalSettings, socialSettings, navLinks }) => (
-  <footer className='Footer'>
-    <div className='container taCenter'>
-      <span>© 2017 All rights reserved.</span>
+  <footer className="Footer">
+    <div className="container taCenter">
+      <span>Francesca Costa © 2018 All rights reserved.</span>
     </div>
   </footer>
 )
